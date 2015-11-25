4d9d20d4795ecf82404b31c053450bf5e520dcbb
Fabiano Andrade Santos Júnior <fabiano.asj@gmail.com>
2015-11-24 22:16:02 -0300

    Teste

--- SistemaAlergiaAlimentar/SistemaAlergiaAlimentar/Program.cs
+++ SistemaAlergiaAlimentar/SistemaAlergiaAlimentar/Program.cs
@@ -14,6 +14,7 @@ namespace SistemaAlergiaAlimentar
         [STAThread]
         static void Main()
         {
+            //Teste
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
